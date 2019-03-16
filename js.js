4c277a01c060205d0b1813da06b9d96af53dc198
skallywag <lee.sam1492@gmail.com>
2019-03-15 22:25:41 -0600

    fixed again

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+//______________STRINGS AND VARIABLES_______________
+
 // var name = "jono" //string are in quotes
 var num = 8 // variables can be numbers, strings, arrays, objects
 // // numbers are values, strings will be whatever the string is
@@ -12,3 +14,22 @@ var age = 26 //numbers can be strings or values
 
 // create a variable for your age and birthday. then create a TEMPLATE LITERAL saying "my name is sam, im 26 and my birthday is 8-14-1992"
 console.log(`my name is ${name}, im ${age} and my birthday is ${birth}`)
+
+//_________________________________________________
+//----------- ARRAYS -------------
+
+var array = []
+var fruits = ['apple', "orange", "pineapple", "grapefruit"]
+
+// An array stores strings, objects, values, and arrays
+
+var array = [] //arrays are always stored in the box []
+var fruits = ['apple', "orange", "pineapple", "grapefruit"]
+//arrays are indexed starting at 0 
+
+fruits.length 
+var names = [`sam`, `kevin`, `caleb` , `dickhead`]
+var dog = `otis`
+console.log(`my name is ${names[0]} my favorite fruit is ${fruits[1]} my dog is ${dog}
+`)
+
