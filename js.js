20714993ef3675c611a23fa43aef66d58b411a48
skallywag <lee.sam1492@gmail.com>
2019-03-15 21:55:54 -0600

    fixed your mistake

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
-hello kevin
-1233333
+// var name = "jono" //string are in quotes
+var num = 8 // variables can be numbers, strings, arrays, objects
+// // numbers are values, strings will be whatever the string is
+
+// console.log('my name is ' + name + ' and im ' + num)
+// console.log(`my name is ${name} and im ${num}`) //ES6 (newer javascript) template literal
+
+
+var name = `sam`
+var birth = `8-14-1995`
+var age = 26 //numbers can be strings or values
+
+// create a variable for your age and birthday. then create a TEMPLATE LITERAL saying "my name is sam, im 26 and my birthday is 8-14-1992"
+console.log(`my name is ${name}, im ${age} and my birthday is ${birth}`)
